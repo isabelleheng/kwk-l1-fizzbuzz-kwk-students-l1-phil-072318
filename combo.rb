695d4c3a6e4c92673b2237073187ad59c975bf0f
isabelleheng <isabellejhheng@gmail.com>
2018-07-24 19:32:41 +0000

    automatically backed up by learn

---
+++ combo.rb
@@ -0,0 +1,16 @@
+def fizzbuzz (int)
+  if int %5 == 0 && int %3 == 0
+    return "fizzbuzz"
+  end
+if int %3 == 0 
+  return "fizz"
+  
+elsif int %5 == 0
+return "buzz"
+
+elsif int %5 == 0 && %3 == 0 
+return "fizzbuzz"
+
+else 
+  return "nil"
+end 
